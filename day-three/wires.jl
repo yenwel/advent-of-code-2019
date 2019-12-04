71a0e2442d4cd05a20cab56b083f6e2a953cea6f
Jan Vandepitte <Jan.Vandepitte@mediahuis.be>
2019-12-04 08:21:21 +0100

    calculate manhattan distance

--- day-three/wires.jl
+++ day-three/wires.jl
@@ -33,10 +33,11 @@ allsame(x) = all(y -> y == first(x), x)
 intersections = function (start1, stop1, start2, stop2)
     # n intersections
     if allsame([start1[1], stop1[1],start2[1],stop2[1]])
-        println("parallel horizontal")
+        println("parallel horizontal $([start1, stop1,start2,stop2])")
     elseif  allsame([start1[2], stop1[2],start2[2],stop2[2]])   
-        println("parallel vertical")
+        println("parallel vertical $([start1, stop1,start2,stop2])")
     end
+
     # 1 intersection
     if isbetween(start1[1], start2[1], stop1[1]) && isbetween(start2[2],start1[2],stop2[2])
         return (true, [(start2[1],start1[2])])
@@ -50,14 +51,14 @@ intersections = function (start1, stop1, start2, stop2)
 end
 
 findintersections = function(wireone,wiretwo)
-    result = Vector{Any}[]
+    result = Tuple{Int64,Int64}[]
     foldl(
         function(start1,stop1) 
             foldl(
                 function(start2, stop2)  
                     intersect = intersections(start1 ,stop1, start2, stop2)
                     if intersect[1]
-                        push!(result, [intersect[2]])
+                        append!(result, intersect[2])
                     end
                     return stop2
                 end,
@@ -68,4 +69,12 @@ findintersections = function(wireone,wiretwo)
     return result
 end
 
-println(findintersections(wire1,wire2))
+intersections = findintersections(wire1,wire2)
+println(intersections)
+
+manhattandistance = (a, b) -> abs(a[1]-b[1]) + abs(a[2] + b[2])
+
+manhattandistancewireintersections = map((x) -> manhattandistance((0,0),x), intersections[2:end])
+
+println(manhattandistancewireintersections)
+println(minimum(manhattandistancewireintersections))
