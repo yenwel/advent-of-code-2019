c9d494234bffcb2bb04e07fb8ae85d853497c3ff
Jan Vandepitte <Jan.Vandepitte@mediahuis.be>
2019-12-04 13:19:25 +0100

    extra criteria

--- day-four/src/main.rs
+++ day-four/src/main.rs
@@ -7,12 +7,24 @@ fn fit_criteria(password : i64) -> bool {
     let mut password_string_vector = Vec::from_iter(password_string.chars());
     password_string_vector.sort();
     let password_string_sorted = password_string_vector.into_iter().collect::<String>();
-    //println!("{} {}", &password_string, &password_string_sorted);
     (
         &password_string == &(password_string_sorted) && 
         password_string.len() == 6 && 
         DOUBLES.iter().any(|&double| (password_string.contains(&double)))
     )
+}
+
+fn extra_criteria(password : i64) -> bool {
+    let password_string = password.to_string();
+    DOUBLES.iter().any(|&double| { 
+        let index = password_string.find(&double);
+        match index {
+            None => false,
+            Some(index) => {
+                ((index == 0 || password_string.chars().nth(index-1) != double.chars().nth(1)) && (index==4 || password_string.chars().nth(index+2) != double.chars().nth(1)))
+            }
+        }
+    })
 }
 
 fn main() {
@@ -23,4 +35,9 @@ fn main() {
     println!("{}", fit_criteria(223450));    
     println!("{}", fit_criteria(123789));
     println!("{}", (235741..706948).into_iter().filter(|x| fit_criteria(*x)).count());
+    println!("{}", extra_criteria(112233));    
+    println!("{}", extra_criteria(123444));    
+    println!("{}", extra_criteria(111122));
+    println!("{}", (235741..706948).into_iter().filter(|x| fit_criteria(*x)).filter(|x| extra_criteria(*x)).count());
+
 }
